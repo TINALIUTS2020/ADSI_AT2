f333a6828a8d57e5285902193d4a13c2a1341354
joshua mccarthy <jxmccarthy@gmail.com>
2023-07-10 19:18:41 +1000

    restructure prod build 2

--- containers/api_prod/app/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uvicorn main:app --proxy-headers --host 0.0.0.0 --port ${PORT}
--- containers/api_prod/app/src/api/Predictor.py
+++
@@ -1,6 +0,0 @@
-from api.DataModels import SingleInput, SingleResponse
-
-async def predict(data: SingleInput):
-    data = data.dict()
-    data["beer_type"] = "test_type"
-    return SingleResponse.parse_obj(data)
