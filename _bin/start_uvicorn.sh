3e9d76ca99be3a7202a92321682ee8072e6b3ca4
joshua mccarthy <jxmccarthy@gmail.com>
2023-07-10 22:16:28 +1000

    update scirpt

--- _bin/start_uvicorn.sh
+++ _bin/start_uvicorn.sh
@@ -1,6 +1,5 @@
 # for running prod
-uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port $INTERNAL_PORT
-
+uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port ${PORT}
 # for running dev
 uvicorn app.main:app --reload --port ${PORT}
 
